9a87f6c09f8874ef0bad8e84546631b14be649ea
Erwan Soulas <erwan.soulas@gmail.com>
2023-05-04 00:24:45 +0200

    init project and backend (server and structure)

---
+++ server/app.js
@@ -0,0 +1,22 @@
+//import modules
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+//Init server
+dotenv.config();
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('API en ligne');
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
+
