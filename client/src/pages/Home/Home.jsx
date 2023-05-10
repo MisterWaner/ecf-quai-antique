b5e5fc48428629ff2bd67ca0ef35a5eba618d258
Erwan Soulas <erwan.soulas@gmail.com>
2023-05-11 00:40:15 +0200

    start home page

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import "./home.css";
+
+const Home = () => {
+    return (
+        <main>
+            <section className="hero-section">
+                <h1 className="main-title">Bienvenue au Quai Antique</h1>
+            </section>
+            <section className="carte-section">
+                
+            </section>
+        </main>
+    )
+};
+
+export default Home;
