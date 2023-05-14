e7be145cf40875d424f52b29d9a3402d2ac827e9
Erwan Soulas <erwan.soulas@gmail.com>
2023-05-14 14:52:56 +0200

    add content for about page and contact page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,13 +1,103 @@
-import './about.css';
+import "./about.css";
 
 const About = () => {
-  return (
-    <>
-    <main>
-        <h1>À Propos</h1>
-    </main>
-    </>
-  )
-}
+    return (
+        <>
+            <main>
+                <h1>À Propos</h1>
+                <section className="about-section">
+                    <h2>Le projet</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aliquam at, exercitationem quidem officiis autem
+                        pariatur quod consequatur sit illo omnis, minus
+                        perspiciatis dolore sed, maiores voluptatem quos ut
+                        mollitia minima? Unde eius, reiciendis quos perferendis
+                        neque odit reprehenderit consectetur et blanditiis
+                        aperiam ipsa itaque, quae numquam. Tempore ratione
+                        dignissimos repellendus laudantium commodi a laboriosam
+                        optio iure atque voluptates voluptas, doloribus illo
+                        sapiente. Est aliquid quam distinctio consequatur magnam
+                        repellendus, minus minima totam ducimus quia corrupti
+                        ipsum perferendis autem deleniti mollitia, dolore
+                        eveniet iste accusamus, non officia itaque! Aspernatur
+                        corrupti modi fuga alias delectus voluptatem error
+                        incidunt eveniet laudantium ipsum facilis et eaque
+                        architecto quasi, velit porro, tempore quibusdam commodi
+                        molestias ducimus in ullam. Minus labore quasi voluptas
+                        praesentium deserunt molestias maiores delectus, qui,
+                        quisquam vero ad quam ut vitae, soluta odio alias. Hic
+                        illo, quia assumenda dolores labore sunt voluptas ullam
+                        quod nam aspernatur officia nemo, inventore asperiores
+                        consequuntur explicabo earum nobis aut suscipit incidunt
+                        numquam iure recusandae blanditiis quos! Quaerat
+                        dignissimos similique doloremque natus autem porro aut
+                        minus sed assumenda esse consectetur ullam, voluptates
+                        nobis, nulla blanditiis provident est quasi ipsum neque
+                        quae non officia distinctio obcaecati. Maiores qui
+                        commodi excepturi eaque ipsam laboriosam eos fuga,
+                        inventore iusto adipisci ducimus facilis labore
+                        necessitatibus assumenda. Quaerat ad neque esse
+                        nesciunt. Vel eius, aliquam voluptatum illo suscipit
+                        eaque dolor beatae provident architecto rerum
+                        perspiciatis ab cupiditate quia libero voluptatibus odit
+                        officiis laudantium assumenda nemo? At dolore nam
+                        voluptates, officia ipsum dicta facere rerum officiis
+                        dolorum nesciunt incidunt accusantium atque et vero.
+                    </p>
+                </section>
+                <section className="about-section">
+                    <h2>Nos horaires d&apos;ouverture</h2>
+                    <table className="planning-table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Jour</th>
+                                <th scope="col">Midi</th>
+                                <th scope="col">Soir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Lundi</th>
+                                <td>Fermé</td>
+                                <td>Fermé</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Mardi</th>
+                                <td>Fermé</td>
+                                <td>19h - 22h</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Mercredi</th>
+                                <td>12h - 14h30</td>
+                                <td>19h - 22h</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Jeudi</th>
+                                <td>12h - 14h30</td>
+                                <td>19h - 22h</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Vendredi</th>
+                                <td>12h - 14h30</td>
+                                <td>19h - 22h</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Samedi</th>
+                                <td>12h - 14h30</td>
+                                <td>19h - 23h</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Dimanche</th>
+                                <td>11h - 15h</td>
+                                <td>Fermé</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </>
+    );
+};
 
-export default About
+export default About;
--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,13 +1,34 @@
-import './contact.css';
+import "./contact.css";
+import Phone from "../../assets/icon/phone-solid.svg";
+import At from "../../assets/icon/at-solid.svg";
+import Location from "../../assets/icon/location-dot-solid.svg";
 
 const Contact = () => {
-  return (
-    <>
-    <main>
-        <h1>Contact</h1>
-    </main>
-    </>
-  )
-}
+    return (
+        <>
+            <main>
+                <h1>Contact</h1>
+                <section className="contact-section">
+                    <div className="phone">
+                        <img src={Phone} alt="" />
+                        <p>04-56-01-02-03</p>
+                    </div>
+                    <div className="adress">
+                        <img src={Location} alt="" />
+                        <address>
+                            <p>Quai Antique</p>
+                            <p>Rue Vieille Monnaie</p>
+                            <p>73000 Chambéry</p>
+                        </address>
+                    </div>
+                    <div className="mail">
+                        <img src={At} alt="" />
+                        <p>contact@quai-antique.fr</p>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+};
 
-export default Contact
+export default Contact;
