f554208d98dc54bed8dc100a8a51839ca0bf956c
Erwan Soulas <erwan.soulas@gmail.com>
2023-05-23 15:13:48 +0200

    start responsive ui of logged user (client)

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,58 @@
 import "./navbar.css";
 import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Logo from "../../assets/Images/logo-quai-antique.png";
+import Burger from "../../assets/icon/burger-solid.svg";
 
 const Navbar = () => {
 
-    const params = useParams()
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
+    };
+
+    const toggleItem = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", changeWidth);
+
+        return () => {
+            window.removeEventListener("resize", changeWidth);
+        };
+    }, []);
+
+    const params = useParams();
     return (
-        <header className="header">
-            <div className="logo-container">
-                <Link to="/">
-                    <img src={Logo} alt="logo du restaurant Quai Antique" />
-                </Link>
-            </div>
-            <nav>
-                <ul className="nav-container">
-                    <li className="nav-item">
-                        <Link to={`/mon-compte/${params.id}`}>Mon Compte</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={`/mon-compte/${params.id}/reservation`}>Réservation</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <nav className="navbar-container">
+            {(toggleMenu || screenWidth > 768) && (
+            <ul className="navbar-list">
+                <li onClick={toggleItem} className="navbar-item logo-container">
+                    <Link to="/">
+                        <img src={Logo} alt="logo du restaurant Quai Antique" />
+                    </Link>
+                </li>
+                <li onClick={toggleItem} className="navbar-item">
+                    <Link to={`/mon-compte/${params.id}`}>Mon Compte</Link>
+                </li>
+                <li onClick={toggleItem} className="navbar-item">
+                    <Link to={`/mon-compte/${params.id}/reservation`}>
+                        Réservation
+                    </Link>
+                </li>
+            </ul>
+            )}
+            <button onClick={toggleNav} className="burger-btn">
+                <img src={Burger} alt="burger icon" />
+            </button>
+        </nav>
     );
 };
 
